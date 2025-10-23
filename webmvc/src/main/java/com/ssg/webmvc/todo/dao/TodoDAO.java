e713736b31cdee4550278a4abb6551a76fc944ec
dynii <dyniiyeyo@naver.com>
2025-10-23 18:02:01 +0900

    251023 lecture

--- webmvc/src/main/java/com/ssg/webmvc/todo/dao/TodoDAO.java
+++ webmvc/src/main/java/com/ssg/webmvc/todo/dao/TodoDAO.java
@@ -1,10 +1,14 @@
 package com.ssg.webmvc.todo.dao;
 
+import com.ssg.webmvc.todo.domain.TodoVO;
 import lombok.Cleanup;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TodoDAO {
     public String getTime(){
@@ -32,5 +36,70 @@ public class TodoDAO {
         return now;
     }
 
+    public void insert(TodoVO vo) throws Exception {
+        String sql = "insert into todo(title, dueDate, finished) values(?, ?, ?)";
 
+        @Cleanup Connection conn = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, vo.getTitle());
+        pstmt.setDate(2, Date.valueOf(vo.getDueDate()));
+        pstmt.setBoolean(3, vo.isFinished());
+        pstmt.executeUpdate();
+    }
+
+    public List<TodoVO> selectAll() throws Exception {
+        String sql = "select * from todo";
+        @Cleanup Connection conn = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement pstmt = conn.prepareStatement(sql);
+        @Cleanup ResultSet rs = pstmt.executeQuery();
+
+        List<TodoVO> list = new ArrayList<>();
+        while (rs.next()) {
+            TodoVO vo = TodoVO.builder()
+                    .tno(rs.getLong("tno"))
+                    .title(rs.getString("title"))
+                    .dueDate(rs.getDate("dueDate").toLocalDate())
+                    .finished(rs.getBoolean("finished"))
+                    .build();
+            list.add(vo);
+        }
+        return list;
+    }
+
+    public TodoVO selectOne(Long tno) throws Exception {
+        String sql = "select * from todo where tno = ?";
+        @Cleanup Connection conn = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setLong(1, tno);
+        @Cleanup ResultSet rs = pstmt.executeQuery();
+        rs.next();
+
+        TodoVO vo = TodoVO.builder()
+                .tno(rs.getLong("tno"))
+                .title(rs.getString("title"))
+                .dueDate(rs.getDate("dueDate").toLocalDate())
+                .finished(rs.getBoolean("finished"))
+                .build();
+        return vo;
+    }
+
+    public void delete(Long tno) throws Exception {
+        String sql = "delete from todo where tno = ?";
+        @Cleanup Connection conn = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setLong(1, tno);
+        pstmt.executeUpdate();
+    }
+
+    public void updateOne(TodoVO vo) throws Exception {
+        String sql = "update todo set title = ?, dueDate = ?, finished = ? where tno = ?";
+        @Cleanup Connection conn = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        pstmt.setString(1, vo.getTitle());
+        pstmt.setDate(2, Date.valueOf(vo.getDueDate()));
+        pstmt.setBoolean(3, vo.isFinished());
+        pstmt.setLong(4, vo.getTno());
+        pstmt.executeUpdate();
+    }
 }
--- webmvc/src/test/java/TodoDAOTests.java
+++ webmvc/src/test/java/TodoDAOTests.java
@@ -1,6 +1,12 @@
 import com.ssg.webmvc.todo.dao.TodoDAO;
+import com.ssg.webmvc.todo.domain.TodoVO;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
 
 public class TodoDAOTests {
     private TodoDAO todoDAO;
@@ -14,4 +20,47 @@ public class TodoDAOTests {
     public void testTime() throws Exception {
         System.out.println(todoDAO.getTime2());
     }
+
+    @Test
+    public void testInsert() throws Exception {
+        TodoVO vo = TodoVO.builder()
+            .title("untitled")
+            .dueDate(LocalDate.now())
+            .finished(true)
+            .build();
+
+        todoDAO.insert(vo);
+    }
+
+    @Test
+    public void testSelectAll() throws Exception {
+        List<TodoVO> list = todoDAO.selectAll();
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelectOne() throws Exception {
+        TodoVO vo = todoDAO.selectOne(1L);
+        System.out.println(vo);
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        todoDAO.delete(1L);
+        Assertions.assertThrows(SQLException.class, () -> {
+            TodoVO vo = todoDAO.selectOne(1L);
+        });
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        TodoVO vo = TodoVO.builder()
+                .tno(1L)
+                .title("untitled1")
+                .dueDate(LocalDate.now())
+                .finished(true)
+                .build();
+        todoDAO.updateOne(vo);
+    }
+
 }
